c6efdfa0fdb4a6494a794bad6b98afb6eed023b0
aprilmca14 <aprilmarie.asiado@bicol-u.edu.ph]>
2015-03-12 17:10:29 +0800

    game initialization
    none

--- clash.py
+++ clash.py
@@ -4,3 +4,13 @@ from pygame.locals import *
 import math
 import random
 import constants
+
+#initialize the game
+def main():
+     pygame.init()
+     width = 800
+     height = 600
+     screen=pygame.display.set_mode((width, height))
+
+if __name__ == "__main__":
+    main()
