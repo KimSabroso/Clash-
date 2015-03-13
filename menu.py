bd67ad1fd215017a0c20d00f02dc5251cb8d670c
aprilmca14 <aprilmarie.asiado@bicol-u.edu.ph]>
2015-03-13 16:57:02 +0800

    load all the necessary images

--- menu.py
+++ menu.py
@@ -21,6 +21,23 @@ class Menu(Frame):
         self.music_sound()
         self.master.title("Clash ++")
 
+        #load images
+        icon = PhotoImage(file = "images/icon.gif")
+        bg = PhotoImage(file = "images/bg.gif")
+        self.sub =PhotoImage(file = "images/sub-bg.gif") 
+        self.new_game_btn = PhotoImage(file = "images/buttons/newgame.gif")
+        self.new_gameh_btn = PhotoImage(file = "images/buttons/newgameh.gif")
+        self.help_btn = PhotoImage(file = "images/buttons/help.gif")
+        self.helph_btn = PhotoImage(file = "images/buttons/helph.gif")
+        self.option_btn = PhotoImage(file = "images/buttons/options.gif")
+        self.optionh_btn =PhotoImage (file = "images/buttons/optionsh.gif")
+        self.hs_btn =PhotoImage (file = "images/buttons/highscore.gif")
+        self.hsh_btn = PhotoImage(file = "images/buttons/highscoreh.gif")
+        self.exit_btn = PhotoImage(file = "images/buttons/exit.gif")
+        self.exith_btn = PhotoImage(file = "images/buttons/exith.gif")
+        self.menu_btn =PhotoImage (file = "images/buttons/menu.gif")
+        self.menuh_btn =PhotoImage (file = "images/buttons/menuh.gif")
+
 win = Tk()
 win.geometry("800x600")
 win.resizable(0,0)
